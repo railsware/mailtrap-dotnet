83b32257ce40d7a2c0dd069527e13734fe3a0be5
Anton Zhaparov <a.zhaparov@outlook.com>
2024-07-09 18:32:44 +0300

    Refactored default API endpoint constants.

--- src/lib/Mailtrap/Constants/Endpoints.cs
+++ src/lib/Mailtrap/Constants/Endpoints.cs
@@ -10,10 +10,28 @@ namespace Mailtrap.Constants;
 
 internal static class Endpoints
 {
-    internal const string RootUrl = "https://mailtrap.io";
-    internal const string ApiRootUrl = "https://api.mailtrap.io";
+    /// <summary>
+    /// Default base URL for Mailtrap API endpoint.<br />
+    /// <see href="https://api-docs.mailtrap.io/docs/mailtrap-api-docs/82708d3cc9606-general"/>
+    /// </summary>
+    internal static string ApiDefaultUrl { get; } = "https://mailtrap.io";
 
-    internal const string SendDefaultUrl = "https://send.api.mailtrap.io";
-    internal const string TestDefaultUrl = "https://test.api.mailtrap.io";
-    internal const string BulkDefaultUrl = "https://bulk.api.mailtrap.io";
+    /// <summary>
+    /// Default base URL for Mailtrap API send endpoint.<br />
+    /// <see href="https://api-docs.mailtrap.io/docs/mailtrap-api-docs/67f1d70aeb62c-send-email-including-templates"/>
+    /// </summary>
+    internal static string SendDefaultUrl { get; } = "https://send.api.mailtrap.io";
+
+    /// <summary>
+    /// Default base URL for Mailtrap API send endpoint.<br />
+    /// <see href="https://api-docs.mailtrap.io/docs/mailtrap-api-docs/711042a37477c-send-email-including-templates"/>
+    /// </summary>
+    internal static string BulkDefaultUrl { get; } = "https://bulk.api.mailtrap.io";
+
+    /// <summary>
+    /// Default base URL for Mailtrap API test endpoint.<br />
+    /// <see href="https://api-docs.mailtrap.io/docs/mailtrap-api-docs/bcf61cdc1547e-send-email-including-templates"/>
+    /// </summary>
+    internal static string TestDefaultUrl { get; } = "https://sandbox.api.mailtrap.io";
+
 }
