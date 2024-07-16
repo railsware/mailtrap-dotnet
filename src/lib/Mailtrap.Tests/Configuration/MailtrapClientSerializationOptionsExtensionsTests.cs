a4c22a11e2a699dff6acd21f9549bdaf42dfd6e9
Anton Zhaparov <a.zhaparov@outlook.com>
2024-07-16 10:52:09 +0300

    Configuration functionality for Mailtrap API client (#5)
    Introduced a set of models to hold API client settings.
    Added some validators for them, so we can ensure proper configuration.
    Added provider for settings, so they can be injected to the client from DI container.
    Added tests for all of the aforementioned.

---
+++ src/lib/Mailtrap.Tests/Configuration/MailtrapClientSerializationOptionsExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MailtrapClientSerializationOptionsExtensionsTests.cs" company="Railsware Products Studio, LLC">
+// Copyright (c) Railsware Products Studio, LLC. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+namespace Mailtrap.Tests.Configuration;
+
+
+[TestFixture]
+internal sealed class MailtrapClientSerializationOptionsExtensionsTests
+{
+    [Test]
+    public void ToJsonSerializerOptions_ShouldThrowArgumentNullException_WhenOptionsAreNull()
+    {
+        MailtrapClientSerializationOptions? options = null;
+
+        var act = () => options!.ToJsonSerializerOptions();
+
+        act.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void ToJsonSerializerOptions_ShouldProduceOptionsBasedOnDefault([Values(false, true)] bool prettyJson)
+    {
+        var options = new MailtrapClientSerializationOptions()
+        {
+            PrettyJson = prettyJson
+        }.ToJsonSerializerOptions();
+
+        var expected = new JsonSerializerOptions(MailtrapJsonSerializerOptions.Default)
+        {
+            WriteIndented = prettyJson
+        };
+
+        options.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ src/lib/Mailtrap.Tests/Configuration/Models/MailtrapClientSerializationOptionsTests.cs
@@ -0,0 +1,36 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MailtrapClientSerializationOptionsTests.cs" company="Railsware Products Studio, LLC">
+// Copyright (c) Railsware Products Studio, LLC. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+namespace Mailtrap.Tests.Configuration.Models;
+
+
+[TestFixture]
+internal sealed class MailtrapClientSerializationOptionsTests
+{
+    [Test]
+    public void Default_ShouldReturnValidDefaults()
+    {
+        MailtrapClientSerializationOptions.Default.PrettyJson.Should().BeFalse();
+    }
+
+    [Test]
+    public void Constructor_ShouldInitValidDefaults()
+    {
+        new MailtrapClientSerializationOptions().PrettyJson.Should().BeFalse();
+    }
+
+    [Test]
+    public void Setter_ShouldCorrectlyChangeField()
+    {
+        var options = MailtrapClientSerializationOptions.Default with
+        {
+            PrettyJson = true
+        };
+
+        options.PrettyJson.Should().BeTrue();
+    }
+}
