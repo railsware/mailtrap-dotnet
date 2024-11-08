0997b1d399335d646cd5cf1c99cedbd78110f7ae
Anton Zhaparov <a.zhaparov@outlook.com>
2024-11-08 08:14:18 +0200

    WiP

---
+++ src/Mailtrap.Abstractions/Models/AccessLevel.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="AccessLevel.cs" company="Railsware Products Studio, LLC">
+// Copyright (c) Railsware Products Studio, LLC. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+
+namespace Mailtrap.Models;
+
+
+/// <summary>
+/// Access level enumeration.
+/// </summary>
+public enum AccessLevel
+{
+    /// <summary>
+    /// None.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Indeterminate.
+    /// </summary>
+    Indeterminate = 1,
+
+    /// <summary>
+    /// Viewer.
+    /// </summary>
+    Viewer = 10,
+
+    /// <summary>
+    /// Admin.
+    /// </summary>
+    Admin = 100,
+
+    /// <summary>
+    /// Owner.
+    /// </summary>
+    Owner = 1000
+}
