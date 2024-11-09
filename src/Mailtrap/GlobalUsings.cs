9378340a9b847b9d3bd63dd68ed60b6ad76ee065
Anton Zhaparov <a.zhaparov@outlook.com>
2024-11-09 08:35:56 +0200

    Adds AccessLevel member. Improves and extends integration tests for account resources.

--- src/Mailtrap/GlobalUsings.cs
+++ src/Mailtrap/GlobalUsings.cs
@@ -10,6 +10,7 @@ global using System.Globalization;
 global using System.Net;
 global using System.Net.Http.Headers;
 global using System.Reflection;
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using FluentValidation;
