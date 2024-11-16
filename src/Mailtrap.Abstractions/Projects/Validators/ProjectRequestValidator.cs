083f6e60d3c7cc0b36604e8a2658484a0417feed
Anton Zhaparov <a.zhaparov@outlook.com>
2024-11-16 10:00:03 +0200

    Improved XMLDoc comments

--- src/Mailtrap.Abstractions/Projects/Validators/ProjectRequestValidator.cs
+++ src/Mailtrap.Abstractions/Projects/Validators/ProjectRequestValidator.cs
@@ -9,17 +9,19 @@ namespace Mailtrap.Projects.Validators;
 
 
 /// <summary>
-/// 
+/// Validator for Create/Update project requests.<br />
+/// Ensures project's name length is within the allowed range.
 /// </summary>
 public sealed class ProjectRequestValidator : AbstractValidator<ProjectRequest>
 {
     /// <summary>
-    /// 
+    /// Static validator instance for reuse.
     /// </summary>
     public static ProjectRequestValidator Instance { get; } = new();
 
+
     /// <summary>
-    /// 
+    /// Primary constructor.
     /// </summary>
     public ProjectRequestValidator()
     {
