2460ea9ba869ae702d8b7d1b15c9bb6f6ef3174f
Anton Zhaparov <a.zhaparov@outlook.com>
2024-11-18 19:00:08 +0200

    Updated .NET SDK version (#39)

--- examples/Mailtrap.Example.SendingDomain/SendingDomain.cs
+++ examples/Mailtrap.Example.SendingDomain/SendingDomain.cs
@@ -1,5 +1,5 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="Permissions.cs" company="Railsware Products Studio, LLC">
+// <copyright file="SendingDomain.cs" company="Railsware Products Studio, LLC">
 // Copyright (c) Railsware Products Studio, LLC. All rights reserved.
 // </copyright>
 // -----------------------------------------------------------------------
